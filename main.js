d87f59a6365ad3f8676e71ee6f132f9d7d989c19
Nicole Apono <napononicole@gmail.com>
2024-04-05 15:51:29 +0300

    main.js

--- main.js
+++ main.js
@@ -6,7 +6,32 @@ document.addEventListener("DOMContentLoaded", function () {
   const errorModal = document.getElementById("modal"); // Gets reference to error modal
 
   // Function to add a click event listener to all the empty hearts
-})
+  document.querySelectorAll(".like-glyph").forEach(function (heart) {
+    heart.addEventListener("click", function () {
+      mimicServerCall()
+      .then(function () {
+        heart.innerText = FULL_HEART; // Changes heart to full heart on success
+        heart.classList.add("activated-heart");
+      })
+      .catch(function (error) {
+      errorModal.classList.remove("hidden"); // Display error modal with message on failure
+      errorModal.innerText = error;
+      // Hide modal after 3 seconds
+      setTimeout(function () {
+        errorModal.classList.add("hidden");
+      }, 3000);
+      });
+    });
+  });
+
+  document.querySelectorAll(".activated-heart").forEach(function (heart) {
+    heart.addEventListener("click", function () {
+      // Change heart back to emoty heart
+      heart.innerText = EMPTY_HEART;
+      heart.classList.remove("activated-heart");
+    });
+  });
+});
 
 
 
